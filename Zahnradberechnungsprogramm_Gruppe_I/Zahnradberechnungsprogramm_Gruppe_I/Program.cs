6d57bb1d2364d363a73f3210e8b5085271dc997a
ru2069 <71378953+ru2069@users.noreply.github.com>
2020-10-19 16:12:41 +0200

    Update Program.cs

--- Zahnradberechnungsprogramm_Gruppe_I/Zahnradberechnungsprogramm_Gruppe_I/Program.cs
+++ Zahnradberechnungsprogramm_Gruppe_I/Zahnradberechnungsprogramm_Gruppe_I/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,40 @@ namespace Zahnradberechnungsprogramm_Gruppe_I
 {
     class Program
     {
+        //Global beständige Konstanten
+        const double Kopfspiel = 0.167;
+        const double Kreiszahl = 3.14;
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Herzlich Willkommen zur pre-Alpha dieses Berechnungsprogramm zur Zahnraddimensionierung.");
+            Console.WriteLine("Bitte geben Sie nun Ihre grundlegenden Werte [Angaben in mm] zur Berechnung ein.");
+            Console.WriteLine("");
+
+            Console.Write("Geben Sie die Zähnezahl ein: ");
+            double zähnezahl = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie den Teilkreisdurchmesser ein: ");
+            double teilkreisdurchmesser = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie die Breite ein: ");
+            double breite = Convert.ToDouble(Console.ReadLine());
+
+
+            //Ausgabe
+            Program prg = new Program();
+
+            double modul = prg.Modul_m(teilkreisdurchmesser, zähnezahl);
+            Console.WriteLine("Das Modul            m = " + modul + "mm");
+
+            Console.ReadKey();
+        }
+
+        public double Modul_m(double teilkreisdurchmesser, double zähnezahl)
+        {
+            double modul = teilkreisdurchmesser / zähnezahl;
+            return modul;
         }
     }
 }
