a1587bef9559baa38d8a2d4e4433115fec1a9ce4
ru2069 <71378953+ru2069@users.noreply.github.com>
2020-10-21 14:55:13 +0200

    Update Program.cs

--- Zahnradberechnungsprogramm_Gruppe_I/Zahnradberechnungsprogramm_Gruppe_I/Program.cs
+++ Zahnradberechnungsprogramm_Gruppe_I/Zahnradberechnungsprogramm_Gruppe_I/Program.cs
@@ -12,11 +12,11 @@ namespace Zahnradberechnungsprogramm_Gruppe_I
     {
         //Global beständige Konstanten
         const double Kopfspiel = 0.167;
-        const double Kreiszahl = 3.14;
-
+        const double Kreiszahl = 3.141;
 
         static void Main(string[] args)
         {
+            //Begrüßung und Parametereingabe
             Console.WriteLine("Herzlich Willkommen zur pre-Alpha dieses Berechnungsprogramm zur Zahnraddimensionierung.");
             Console.WriteLine("Bitte geben Sie nun Ihre grundlegenden Werte [Angaben in mm] zur Berechnung ein.");
             Console.WriteLine("");
@@ -29,13 +29,43 @@ namespace Zahnradberechnungsprogramm_Gruppe_I
 
             Console.Write("Geben Sie die Breite ein: ");
             double breite = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
 
+            //Berechnungsauswahl
+            Console.WriteLine("Nun haben Sie die Möglichkeit zwischen geradeverzahnten oder schrägverzahnten Stirnrädern zu wählen.");
+            Console.Write("Bitte geben Sie '1' für Geradverzahnung oder '2' für Schrägverzahnung ein: ");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
 
-            //Ausgabe
+            //Ausgabe Geradverzahnung allgemein
             Program prg = new Program();
+            if (a == 1)
+            {
+                //Berechnungsauswahl in Geradverzahnung
+                Console.WriteLine("Bitte wählen Sie zwischen Außen- oder Innenverzahnung.");
+                Console.Write("Bitte geben Sie '1' für Außenverzahnung oder '2' für Innenverzahnung ein: ");
+                int b;
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
 
-            double modul = prg.Modul_m(teilkreisdurchmesser, zähnezahl);
-            Console.WriteLine("Das Modul            m = " + modul + "mm");
+                if (b == 1)
+                {
+                    double modul = prg.Modul_m(teilkreisdurchmesser, zähnezahl);
+                    Console.WriteLine("Das Modul            m = " + modul + "mm");
+                }
+                else
+                {
+                    double modul = prg.Modul_m(teilkreisdurchmesser, zähnezahl);
+                    Console.WriteLine("Das TEST            m = " + modul + "mm");
+                }
+            }
+
+            //Ausgabe Schrägverzahnung
+            else
+            {
+                Console.WriteLine("jbvhjdn");
+            }
 
             Console.ReadKey();
         }
