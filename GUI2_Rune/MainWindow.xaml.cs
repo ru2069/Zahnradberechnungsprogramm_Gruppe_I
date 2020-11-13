9383e56b16138a6e58416324d627239f574af0c8
ru2069 <71378953+ru2069@users.noreply.github.com>
2020-11-13 21:53:21 +0100

    GUI2

---
+++ GUI2_Rune/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI2_Rune
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double m = Convert.ToDouble(modul.Text);
+            double z = Convert.ToDouble(zähnezahl.Text);
+
+            double d = m * z;
+            teilkreisdurchmesser.Content = d;
+        }
+    }
+}
