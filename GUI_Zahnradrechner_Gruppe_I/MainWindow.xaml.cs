291d32267cb02bae0bba1ec9d6c59fa33b903f7a
ru2069 <71378953+ru2069@users.noreply.github.com>
2020-11-17 18:37:52 +0100

    Update MainWindow.xaml.cs

--- GUI_Zahnradrechner_Gruppe_I/MainWindow.xaml.cs
+++ GUI_Zahnradrechner_Gruppe_I/MainWindow.xaml.cs
@@ -25,13 +25,75 @@ namespace GUI_Zahnradrechner_Gruppe_I
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            double m = Convert.ToDouble(modul.Text);
-            double z = Convert.ToDouble(zähnezahl.Text);
 
-            double d = m * z;
-            teilkreisdurchmesser.Content = d;
+        //AUßENVERZAHNUNG
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //if (radiobutton gerade == true)
+                string zahlCheckModul = modul.Text;
+                string zahlCheckZähnezahl = zähnezahl.Text;
+
+                if (eingabecheckmodul(zahlCheckModul) == true)
+                {
+
+                if (eingabecheckzähnezahl(zahlCheckZähnezahl) == true)
+                {
+                    //If geradezahlige Zähnezal
+                        double m = Convert.ToDouble(modul.Text);
+                        double z = Convert.ToDouble(zähnezahl.Text);
+
+                        double d = m * z;
+                        teilkreisdurchmesser.Content = d;
+                    //else if
+                        //Fehler
+                }
+                else if (eingabecheckzähnezahl(zahlCheckZähnezahl) == false)
+                {
+                    MessageBox.Show("Sie müssen eine Zahl als Zähnezahl eingeben!");
+                }
+                }
+                else if (eingabecheckmodul(zahlCheckModul) == false)
+                {
+                MessageBox.Show("Sie müssen eine Zahl als Modul eingeben!");
+                }
+
+            //else if (radiobutton schräg == true)
+                //if
+            
+        }
+
+        //INNENVERZAHNUNG
+        private void btn_ClickInnen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private bool eingabecheckmodul(string zahlCheckModul)
+        {
+            try
+            {
+                double eingabeZahl = double.Parse(zahlCheckModul);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+        private bool eingabecheckzähnezahl(string zahlCheckZähnezahl)
+        {
+            try
+            {
+                double eingabeZahl = double.Parse(zahlCheckZähnezahl);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
         }
     }
 }
