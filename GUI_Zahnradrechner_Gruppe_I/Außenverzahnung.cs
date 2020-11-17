57eb0afc7208a5ab588e368022b36c0455cfa80b
christoeph <72197458+christoeph@users.noreply.github.com>
2020-11-17 23:30:30 +0100

    Update Außenverzahnung.cs

--- GUI_Zahnradrechner_Gruppe_I/Außenverzahnung.cs
+++ GUI_Zahnradrechner_Gruppe_I/Außenverzahnung.cs
@@ -8,32 +8,7 @@ namespace GUI_Zahnradrechner_Gruppe_I
     {
         public void geradverzahnungAußen()
         {
-            string zahlCheckModul = modul.Text;
-            string zahlCheckZähnezahl = zähnezahl.Text;
 
-            if (eingabecheckmodul(zahlCheckModul) == true)
-            {
-
-                if (eingabecheckzähnezahl(zahlCheckZähnezahl) == true)
-                {
-                    //If geradezahlige Zähnezal
-                    double m = Convert.ToDouble(modul.Text);
-                    double z = Convert.ToDouble(zähnezahl.Text);
-
-                    double d = m * z;
-                    teilkreisdurchmesser.Content = d;
-                    //else if
-                    //Fehler
-                }
-                else if (eingabecheckzähnezahl(zahlCheckZähnezahl) == false)
-                {
-                    MessageBox.Show("Sie müssen eine Zahl als Zähnezahl eingeben!");
-                }
-            }
-            else if (eingabecheckmodul(zahlCheckModul) == false)
-            {
-                MessageBox.Show("Sie müssen eine Zahl als Modul eingeben!");
-            }
         }
     }
 }
