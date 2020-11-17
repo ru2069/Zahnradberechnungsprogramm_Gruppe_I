626eb3e3c1495ba0de7ffe6b62411cf82a553a5c
christoeph <72197458+christoeph@users.noreply.github.com>
2020-11-17 23:16:24 +0100

    Update MainWindow.xaml.cs

--- GUI_Zahnradrechner_Gruppe_I/MainWindow.xaml.cs
+++ GUI_Zahnradrechner_Gruppe_I/MainWindow.xaml.cs
@@ -36,36 +36,11 @@ namespace GUI_Zahnradrechner_Gruppe_I
             Az.geradverzahnungAußen();
 
 
-            string zahlCheckModul = modul.Text;
-                string zahlCheckZähnezahl = zähnezahl.Text;
-
-                if (eingabecheckmodul(zahlCheckModul) == true)
-                {
-
-                if (eingabecheckzähnezahl(zahlCheckZähnezahl) == true)
-                {
-                    //If geradezahlige Zähnezal
-                        double m = Convert.ToDouble(modul.Text);
-                        double z = Convert.ToDouble(zähnezahl.Text);
-
-                        double d = m * z;
-                        teilkreisdurchmesser.Content = d;
-                    //else if
-                        //Fehler
-                }
-                else if (eingabecheckzähnezahl(zahlCheckZähnezahl) == false)
-                {
-                    MessageBox.Show("Sie müssen eine Zahl als Zähnezahl eingeben!");
-                }
-                }
-                else if (eingabecheckmodul(zahlCheckModul) == false)
-                {
-                MessageBox.Show("Sie müssen eine Zahl als Modul eingeben!");
-                }
 
             //else if (radiobutton schräg == true)
                 //if
-            
+            //Schrägverzahnung Sz = new Schrägverzahnung();
+            //Sz.schrägverzahnungAußen();
         }
 
         //INNENVERZAHNUNG
